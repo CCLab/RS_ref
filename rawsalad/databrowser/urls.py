053590678387b34c419e9e8ef96e2af072d60bbb
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-12-08 16:31:12 +0100

    typo in urls

--- rawsalad/databrowser/urls.py
+++ rawsalad/databrowser/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns( 'databrowser.views',
-    (r'^$', 'app_page' ),
-    (r'^(?P<idef>\d+)/$', 'init_restore' ),
-    (r'^store_state/$', 'store_state' ),
-    (r'^restore_state/$', 'restore_state' ),
-    (r'^prepare_data/$', 'prepare_data' ),
-    (r'^download/$', 'download_data' ),
-    (r'^feedback/$', 'feedback_email' ),
-    (r'^search/$', 'search_data' ),
-    (r'^get_searched/$', 'get_searched_data' ),
+    ( r'^$', 'app_page' ),
+    ( r'^(?P<idef>\d+)/$', 'init_restore' ),
+    ( r'^store_state/$',   'store_state' ),
+    ( r'^restore_state/$', 'restore_state' ),
+    ( r'^prepare_data/$',  'prepare_data' ),
+    ( r'^download/$',      'download_data' ),
+    ( r'^feedback/$',      'feedback_email' ),
+    ( r'^search/$',        'search_data' ),
+    ( r'^get_searched/$',  'get_searched_data' ),
 )
