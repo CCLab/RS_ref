6a65e61fd5fd7d4891f2ee50e23d0f0dffe274cc
Bartosz Stalewski <bartekstalewski@gmail.com>
2012-03-13 13:03:45 +0100

    Hierarchy upload script

---
+++ scripts/postgresql_upload/test_hierarchy.sql
@@ -0,0 +1,17 @@
+DROP TABLE hierarchy CASCADE;
+CREATE TABLE hierarchy (
+    endpoint        varchar(20),
+    nr              integer,
+    label           varchar(100),
+    aux             boolean DEFAULT FALSE,
+    aux_level       varchar(100),
+    PRIMARY KEY (endpoint, nr)
+);
+
+DELETE FROM hierarchy;
+BEGIN;
+INSERT INTO hierarchy VALUES( 'data_50001', 1,  'Część', TRUE, 'Numer części');
+INSERT INTO hierarchy VALUES( 'data_50001', 2,  'Część', TRUE, 'Numer części');
+INSERT INTO hierarchy VALUES( 'data_50001', 3,  'Dział', TRUE, 'Numer działu');
+INSERT INTO hierarchy VALUES( 'data_50001', 4,  'Rozdział', TRUE, 'Numer rozdziału');
+COMMIT;
